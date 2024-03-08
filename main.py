39b1ec009b03948188383042458a498c5ff6693a
Sqfav <igor.mozzy@outlook.com>
2024-03-08 16:20:08 +0100

    Версия с измененной логикой

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
+from abc import ABC
+
+
 class Document:
     def __init__(self, text):
         self.text = text
 
     def print_document(self, printer):
-        printer.print_page(self.text)
+        printer.print_page(printer, self.text)
 
 
-class InkjetPrinter:
+class Printer(ABC):
     @staticmethod
-    def print_page(text):
-        print("Device: Inkjet Printer:")
-        print(text)
+    def print_page(self, text):
+        print(f'Device: {self.name}')
+        print(self.style(text))
 
 
-class LaserPrinter:
+class InkjetPrinter(Printer):
+    name = 'Inkjet Printer'
     @staticmethod
-    def print_page(text):
-        print("Device: Laser Printer:")
-        print(text)
+    def style(text):
+        return text.upper()
+
+
+class LaserPrinter(Printer):
+    name = 'Laser Printer'
+    @staticmethod
+    def style(text):
+        return text.lower()
+
+class HPLaserJet(Printer):
+    name = 'HPLaserJet Pro 2200'
+    @staticmethod
+    def style(text):
+        return text.swapcase()
 
 
 if __name__ == "__main__":
     document = Document("Text of the document.")
-
-    inkjet_printer = InkjetPrinter()
-    laser_printer = LaserPrinter()
-
-    document.print_document(inkjet_printer)
-    print("-----------")
-    document.print_document(laser_printer)
+    try:
+        subclasses = Printer.__subclasses__()
+        for i, sub in enumerate(subclasses, start=1):
+            print(f'{i}: {sub.name}')
+        choice = subclasses[int(input('Введи номер выбранного принтера: '))-1]
+    except (ValueError, IndexError):
+        print('Можно вводить только число, соответствующее номеру принтера. До-сви-да-ни-я.')
+    else:
+        document.print_document(choice)
--- test_main.py
+++ test_main.py
@@ -1,27 +1,31 @@
 import unittest
 import io
 from unittest.mock import patch
-from main import Document, InkjetPrinter, LaserPrinter
+from main import Document, InkjetPrinter, LaserPrinter, HPLaserJet
 
 
 class TestDocumentPrinting(unittest.TestCase):
     def setUp(self):
-        self.inkjet_printer = InkjetPrinter()
-        self.laser_printer = LaserPrinter()
-        self.text = "string for tests"
-        self.document = Document(self.text)
+        self.document_text = "yet another test string"
+        self.document = Document(self.document_text)
 
     def test_inkjet_printing(self):
-        expected_output = "Device: Inkjet Printer:\n" + self.text + "\n"
-        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            self.document.print_document(self.inkjet_printer)
-            assert mock_stdout.getvalue() == expected_output
+        expected_output = f'Device: Inkjet Printer\n{self.document_text.upper()}\n'
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.document.print_document(InkjetPrinter)
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
     def test_laser_printing(self):
-        expected_output = "Device: Laser Printer:\n" + self.text + "\n"
-        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            self.document.print_document(self.laser_printer)
-            assert mock_stdout.getvalue() == expected_output
+        expected_output = f'Device: Laser Printer\n{self.document_text.lower()}\n'
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.document.print_document(LaserPrinter)
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_hp_laserjet_printing(self):
+        expected_output = f'Device: HPLaserJet Pro 2200\n{self.document_text.swapcase()}\n'
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.document.print_document(HPLaserJet)
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
 
 if __name__ == "__main__":
